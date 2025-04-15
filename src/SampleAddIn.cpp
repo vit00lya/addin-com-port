511df6cf0cc075be05f0b9d47c73c9af07c7820d
Vitaly Sandalov <vit00lya@yandex.ru>
2025-04-15 23:27:40 +0300

    First commit

---
+++ src/SampleAddIn.cpp
@@ -0,0 +1,57 @@
+﻿/*
+ *  Modern Native AddIn
+ *  Copyright (C) 2018  Infactum
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+#include <chrono>
+#include <iomanip>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <thread>
+
+#include "SampleAddIn.h"
+
+std::string SampleAddIn::extensionName() {
+    return "ComPortTools";
+}
+
+SampleAddIn::SampleAddIn() {
+
+    // Full featured property registration example
+    AddProperty(L"Version", L"ВерсияКомпоненты", [&]() {
+        auto s = std::string(Version);
+        return std::make_shared<variant_t>(std::move(s));
+    });
+
+    // Method registration.
+    // Lambdas as method handlers are not supported.
+    AddMethod(L"GetLine", L"ПолучитьСтроку", this, &SampleAddIn::GetLine);
+
+}
+
+
+variant_t SampleAddIn::GetLine() {
+    xserial::ComPort com = xserial::ComPort(
+                            0,
+                          baud_rate_,
+                          parity_,
+                          data_bits_,
+                          stop_bit_);
+    std::string tmp = com.getLine();
+    return tmp;
+}
